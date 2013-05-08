382de526568a6b553e44812a0c9dde94d52f1416
Photon Infotech <maretska@Photons-Mac-mini-104.local>
2013-05-08 19:48:07 +0700

    Add Robotium Connect To Door
    AndroidConnectToDoorTest

---
+++ AndroidConnectToDoorTest/gen/com/photon/connecttodoor/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.photon.connecttodoor.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ AndroidConnectToDoorTest/src/com/photon/connecttodoor/test/Athendance.java
@@ -0,0 +1,57 @@
+package com.photon.connecttodoor.test;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.jayway.android.robotium.solo.Solo;
+import com.photon.connecttodoor.LoginActivity;
+import com.photon.connecttodoor.R;
+
+public class Athendance extends ActivityInstrumentationTestCase2<LoginActivity> {
+	
+	private Solo solo;
+	boolean expected = true;
+	   boolean actual;
+	
+	public Athendance() {
+	               super(LoginActivity.class);
+
+	       }
+
+	       @Override
+	       public void setUp() throws Exception {
+
+	               super.setUp();
+	               solo = new Solo(getInstrumentation(), getActivity());
+
+	       }
+
+	       @Override
+	       public void tearDown() throws Exception {
+
+	               try {
+	                       solo.finalize();
+	               } catch (Throwable e) {
+	                       // TODO: handle exception
+	                       e.printStackTrace();
+	               }
+	               getActivity().finish();
+	               solo.finishOpenedActivities();
+	               super.tearDown();
+	       }
+	       
+	       
+	       public void testLoginFb(){
+	    	   solo.clickOnButton(0);
+	    	   actual = solo.searchText("Welcome");
+	    	   assertEquals(expected, actual);
+	    	   
+	       }
+	       
+	       public void testProfile(){
+	    	   solo.clickOnButton(0);
+	    	   solo.clickOnButton("Voucher");
+	    	   actual = solo.searchButton("Attendace");
+	    	   assertEquals(expected, actual);
+	       }
+	       
+}
